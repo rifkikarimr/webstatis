54d97928f4b9d9ed070ed2447a2865d22aa0da14
rifkikarimr <rifkikarimr@gmail.com>
2024-01-16 15:16:10 +0700

    initial commit

--- public/server.js
+++ public/server.js
@@ -1,9 +1,9 @@
-const express = require('express');
 const app = express();
-const port = 3000;
+const port = 8080;
+const host = '0.0.0.0';
+
 
 app.use(express.static('public')); // This serves static files from the 'public' directory
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+app.listen(PORT, HOST);
+console.log(`Running on http://${host}:${port}`);
