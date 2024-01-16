5a56afaeb7aacf638fbc51c01fa71a55220ac3ae
rifkikarimr <rifkikarimr@gmail.com>
2024-01-16 16:23:15 +0700

    initial commit

--- public/server.js
+++ public/server.js
@@ -5,5 +5,6 @@ const host = '0.0.0.0';
 
 app.use(express.static('public')); // This serves static files from the 'public' directory
 
-app.listen(PORT, HOST);
+app.listen(port, host);
+
 console.log(`Running on http://${host}:${port}`);
