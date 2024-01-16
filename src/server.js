468cfe59fec46f87da6e75db0fc8fe2565722473
rifkikarimr <rifkikarimr@gmail.com>
2024-01-16 22:21:51 +0700

    New commit

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 8080;
 // const host = '0.0.0.0';
 
 
-app.use(express.static('public')); // This serves static files from the 'public' directory
+app.use(express.static('src')); // This serves static files from the 'public' directory
 
 
 app.listen(port, () => {
